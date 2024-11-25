b0e456d3985ab13a0af9d82d8697b241e94279de
OlegBon <oleg_bon@ukr.net>
2024-11-25 14:29:15 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import "./App.css";
+// import { app } from "./firebase-config";
+
+function App() {
+  // console.log(app); // Перевіряємо app
+  return <div className="App"></div>;
+}
+
+export default App;
